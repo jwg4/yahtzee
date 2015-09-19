74d73398bb9b6ff81704b7aa88af0e894cb4c7f1
Jack <jack.grahl@yahoo.co.uk>
2015-09-19 12:14:42 +0100

    Call the API 5 times, and print the output.

--- yahtzee.py
+++ yahtzee.py
@@ -1,4 +1,9 @@
 import requests
 
-r = requests.get('http://rollthedice.setgetgo.com/get.php')
-print r.text
+l = []
+
+for i in range(5):
+    r = requests.get('http://rollthedice.setgetgo.com/get.php')
+    l.append(r.text)
+
+print " ".join(l)
