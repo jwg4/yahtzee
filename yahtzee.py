bcc5f4baad10725f83a4b2036f23992f818f947a
Jack <jack.grahl@yahoo.co.uk>
2015-10-13 10:07:35 +0100

    Move the code to roll the dice into a function.

--- yahtzee.py
+++ yahtzee.py
@@ -1,9 +1,13 @@
 import requests
 
-l = []
+def get_dice_rolls():
+    l = []
 
-for i in range(5):
-    r = requests.get('http://rollthedice.setgetgo.com/get.php')
-    l.append(r.text)
+    for i in range(5):
+        r = requests.get('http://rollthedice.setgetgo.com/get.php')
+        l.append(r.text)
 
-print " ".join(l)
+    return l
+
+result = get_dice_rolls()
+print " ".join(result)
